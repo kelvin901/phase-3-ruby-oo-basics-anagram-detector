d924d849a6e5fca6bc3b080ff90cedc72675b816
kelvin901 <wawerukelvin2019@gmail.com>
2023-05-19 16:16:38 +0300

    Uncomplete Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,6 @@
-# Your code goes here!
+# Your code goes herclass Anagram: This is a pythoncode
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        return [word for word in words if sorted(self.word) == sorted(word)]
